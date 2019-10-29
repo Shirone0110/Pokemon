f6171c0e3be6eac99de92918b34047af8b5e6902
Shirone0110 <lam.le@centre.edu>
2019-10-29 11:34:15 -0400

    added icons

--- poke.js
+++ poke.js
@@ -1,5 +1,13 @@
-var pokenamePromise = d3.json("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=50");
-var pokeabilityPromise = d3.json("https://pokeapi.co/api/v2/ability/?offset=20&limit=50");
+var pokenamePromise = d3.json("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=50");
+var pokeabilityPromise = d3.json("https://pokeapi.co/api/v2/ability/?offset=0&limit=50");
+
+d3.select("#name").attr("clickcount", 0);
+d3.select("#id").attr("clickcount", 0);
+d3.select("#exp").attr("clickcount", 0);
+d3.select("#height").attr("clickcount", 0);
+d3.select("#weight").attr("clickcount", 0);
+
+var header = ["#name", "#id", "#exp", "#height", "#weight"];
 
 pokenamePromise.then( //get pokemon information
 function (pokemons)
@@ -15,6 +23,7 @@ function (pokemons)
         console.log(poke);
         drawTable(poke);
         makeHeader(poke);
+        Row(poke, poke);
     })
 },
 function (err)
@@ -36,9 +45,30 @@ var sortCol = function(pokemons, col, accessor)
     d3.select(col)
         .on("click", function()
         {
+            var x = parseInt(d3.select(col).attr("clickcount"), 10);
+            d3.select(col).attr("clickcount", (x + 1) % 3);
+            header.forEach(function(item, index)
+            {
+                if (item != col) d3.select(item).attr("clickcount", 0);
+                var cc = d3.select(item).attr("clickcount");
+                var cell = d3.select(item);
+                cell.selectAll("img").remove();
+                if (cc == 1)
+                {
+                    cell.append("img")
+                    .attr("src", "asc.png");
+                }
+                else if (cc == 2)
+                {
+                    cell.append("img")
+                        .attr("src", "desc.png");
+                }
+            })
             pokemons.sort(function(a, b)
             {
-                return (accessor(a) - accessor(b));
+                var cc = d3.select(col).attr("clickcount");
+                if (cc == 2) return (accessor(b) - accessor(a));
+                else return (accessor(a) - accessor(b));
             })
             drawTable(pokemons);
         })
@@ -65,6 +95,89 @@ var addCol = function(rows, txt) //add column
 {
     rows.append("td")
         .text(txt);
+}
+
+var show = function(pokemons, original, index)
+{
+    var box = d3.select("body");
+    var poke = original[index - 1];
+    
+    box.append("h1")
+        .text(poke.name);
+    
+    box.append("h3")
+        .text("Abilities");
+    
+    box.append("ul")
+        .selectAll("li")
+        .data(poke.abilities)
+        .enter()
+        .append("li")
+        .text(function(d)
+        {
+            return d.ability.name;
+        })
+    
+    box.append("h3")
+        .text("Base experience")
+    
+    box.append("p")
+        .text(poke.base_experience)
+    
+    box.append("h3")
+        .text("Forms")
+    
+    box.append("ul")
+        .selectAll("li")
+        .data(poke.forms)
+        .enter()
+        .append("li")
+        .text(function(d)
+        {
+            return d.name;
+        })
+    
+    box.append("h3")
+        .text("Height")
+    
+    box.append("p")
+        .text(poke.height)
+    
+    box.append("h3")
+        .text("Stats")
+    
+    box.append("ol")
+        .selectAll("li")
+        .data(poke.stats)
+        .enter()
+        .append("li")
+        .text(function(d)
+        {
+            return d.stat.name + ": " + d.base_stat;
+        })
+    
+    box.append("h3")
+        .text("Types")
+    
+    box.append("ol")
+        .selectAll("li")
+        .data(poke.types)
+        .enter()
+        .append("li")
+        .text(function(d)
+        {
+            return d.type.name + ": " + d.slot + " slot";
+        })
+}
+
+var Row = function(pokemons, original)
+{
+    d3.selectAll("td")
+        .on("click", function(d)
+    {
+        d3.selectAll("body *").remove();
+        show(pokemons, original, d.id);
+    })
 }
 
 var drawTable = function (poke)
